a171e2e9c786c0cd86aa5ca57dd4880138f36a58
KevinShih <kevin061215@gmail.com>
2022-07-20 16:50:13 +0800

    feat(Home): Edit home component

--- src/pages/home/components/index.js
+++ src/pages/home/components/index.js
@@ -1,39 +1,34 @@
-import React, {useState, useEffect, Suspense} from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { Stack } from "@mui/material";
-import { getCustomList } from "../utils/data_transformer"
+import { getCustomList } from "../utils/data_transformer";
 
-const TestCP = React.lazy(() => import('./test/'))
+const TestCP = React.lazy(() => import("./test/"));
 
 const Home = () => {
+  const [customList, setCustomList] = useState([]);
 
-    const [ customList, setCustomList ] = useState(null)
+  useEffect(() => {
+    getCustomList()
+      .then((res) => {
+        setCustomList(res);
+      })
+      .catch((error) => {
+        console.error(`Get customer list ERROR: `, error);
+      });
+  }, []);
 
-    useEffect(() => {
-        getCustomList().then((res) => {
-            console.log(`res: `, res)
-            setTimeout(() => {
-                setCustomList(res)
-            }, 5000);
-        }).catch((error) => {
-            console.error(`Get customer list ERROR: `, error)
-        })
-    }, [])
+  return (
+    <Stack className="component-app">
+      <h1>Home Page</h1>
+      {/* Use Suspense can add the fallback JSX if the inside JSX is not render yet. */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <TestCP />
+        {customList.map((c) => (
+          <li>{c.name}</li>
+        ))}
+      </Suspense>
+    </Stack>
+  );
+};
 
-    return(
-        <Stack >
-            <h1>Home Page</h1>
-            {/* Use Suspense can add the fallback JSX if the inside JSX is not render yet. */}
-            <Suspense fallback={<div>Loading...</div>}>
-                <TestCP />
-                    {
-                        customList !== null&&
-                        customList.map(c => 
-                            <li>{c.name}</li>
-                        )
-                    }
-            </Suspense>
-        </Stack>
-    )
-}
-
-export default Home
+export default Home;
