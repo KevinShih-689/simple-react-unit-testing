dc037b4a28233d085d4411d22cb99eef5bfb220f
KevinShih <kevin061215@gmail.com>
2022-07-20 16:49:00 +0800

    feat(data_transformer): Add remark

--- src/pages/home/utils/data_transformer.js
+++ src/pages/home/utils/data_transformer.js
@@ -1,11 +1,30 @@
 import React, {useState} from "react";
 import { USER_LIST } from "../mock_data/"
 
+/**
+ * Get Custom List from mock data.
+ * @function getCustomList
+ * @returns {UserList}
+ */
 const getCustomList = async () => {
     /* Data Transform */
     return USER_LIST
 }
 
+/**
+ * Simple sum function
+ * @param {Number} number1 - The first number
+ * @param {Number} number2 - The second number
+ * @returns {Sum} 
+ */
+const sumNumber = (number1, number2) => {
+    let sum = number1 + number2
+
+    return sum;
+}
+
+
 export { 
-    getCustomList
+    getCustomList,
+    sumNumber
 }
